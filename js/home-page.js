5441da3b287e9729cfc65f0b86391329d1454488
Mari018 <andrepinto695@gmail.com>
2025-03-04 19:07:14 +0000

    agora sim

--- js/home-page.js
+++ js/home-page.js
@@ -6,3 +6,17 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 });
+
+document.addEventListener("click", (event) => {
+	// Verifica se o clique foi fora do menu e do botão
+	if (
+		!hamMenu.contains(event.target) &&
+		!offScreenMenu.contains(event.target)
+	) {
+		// Remove a classe "active" se o menu estiver aberto
+		if (offScreenMenu.classList.contains("active")) {
+			hamMenu.classList.remove("active");
+			offScreenMenu.classList.remove("active");
+		}
+	}
+});
